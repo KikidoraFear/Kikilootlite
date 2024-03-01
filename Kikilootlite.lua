4315abfa729b958cdd28f5bde608635066ef006f
KikidoraFear <154637862+KikidoraFear@users.noreply.github.com>
2024-03-01 18:53:11 +0100

    minor changes, nothing of importance!

--- Kikilootlite.lua
+++ Kikilootlite.lua
@@ -998,34 +998,56 @@ end)
 -- # ONUPDATE AND EVENTS #
 -- #######################
 
+local motivator_machine = {"It's never too late to give up",
+    "This is the worst day of my life",
+    "Don't follow your friends off a bridge; lead them",
+    "Just because you're special, doesn't mean you're useful",
+    "No one is as dumb as all of us together",
+    "It may be that the purpose of your life is to serve as a warning for others",
+    "If you ever feel alone, don't",
+    "Give up on your dreams and die",
+    "Trying is the first step to failure",
+    "Make sure to drink water so you can stay hydrated while you suffer",
+    "The Nail that sticks out gets hammered down",
+    "I got an ant farm. They didn't grow shit",
+    "They don't think it be like it is but it do",
+    "If Id agreed with you we'd both be wrong",
+    "Tutant meenage neetle teetle",
+    "When you want win but you receive lose",
+    "Get two birds stoned at once",
+    "Osteoporosis sucks",
+    "Success is just failure that hasn't happened yet",
+    "Never underestimate the power of stupid people in large groups",
+    "I hate everyone equally",
+    "Only dread one day at a time",
+    "Hope is the first step on the road to disappointment",
+    "The beatings will continue until morale improves",
+    "It's always darkest just before it goes pitch black",
+    "When you do bad, no one will forget",
+    "Life's a bitch, then you die",
+    "You suck",
+    "Fuck you",
+    "Not even Noah's ark can carry you, animals",
+    "Your mother buys you Mega Bloks instead of Legos",
+    "You look like you cut your hair with a knife and fork",
+    "You all reek of poverty and animal abuse",
+    "Your garden is overgrown and your cucumbers are soft"
+}
+
 window:RegisterEvent("LOOT_OPENED")
--- window:RegisterEvent("CHAT_MSG_RAID")
+window:RegisterEvent("PLAYER_DEAD")
 -- window:RegisterEvent("CHAT_MSG_PARTY")
 -- window:RegisterEvent("CHAT_MSG_RAID_LEADER")
 -- window:RegisterEvent("CHAT_MSG_RAID_WARNING")
 window:SetScript("OnEvent", function()
-    -- if event=="LOOT_OPENED" then
+    if event=="LOOT_OPENED" then
         if UnitName("player")==loot_master then
             ShowLoot(data_sr, data_ss, data_if)
         end
-    -- else -- arg1=item_link, arg2=author; add info to item if linked in raid
-    --     if arg2==loot_master then
-    --         local item_string = GetItemStringFromItemlink(item_link)
-    --         local item_name,_ = GetItemInfo(item_string)
-    --         local item_ss = data_ss[item_name] or ""
-    --         local item_sr = ""
-    --         if data_sr[item_name] then
-    --             for _, player_name in pairs(data_sr[item_name]) do
-    --                 item_sr = item_sr..player_name.." "
-    --                 SendChatMessage("Hey, you have SR on "..arg1, "WHISPER", nil, player_name);
-    --             end
-    --         end
-    --         if (item_ss ~= "") or (item_sr ~="") then
-    --             local text = item_ss.." ("..item_sr..")"
-    --             SendChatMessage(text , config.channel, nil, nil)
-    --         end
-    --     end
-    -- end
+    else
+        local idx = math.random(1, GetTableLength(motivator_machine))
+        SendChatMessage(motivator_machine[idx] , "RAID_WARNING", nil, nil)
+    end
 end)
 
 -- check loot master
